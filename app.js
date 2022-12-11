c49dc2368d02d4726f135dc81a175fc44a76fe3c
Hira Muazzam <hiramuazzam172@gmail.com>
2022-12-12 01:59:27 +0500

    image to base 64

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 require("dotenv").config();   
-var mongoUtil = require( './mongoUtil.js' );
 var scrapping = require('./scrapping.js')
+const fs = require('fs/promises');
 
-mongoUtil.connectToServer(async function( err, client ) {
-    var manager = require('./mongoManger')
-    var result = await scrapping("21231")
+var request = require('request').defaults({ encoding: null });
+
+async function imageConvert(url){
+    await request.get(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            data = "data:" + response.headers["content-type"] + ";base64," + Buffer.from(body).toString('base64');
+            return data;
+        }
+        else    
+            console.log(error)
+    });
+}
+
+
+
+async function processing(){
+    // var result = await scrapping("21231")
+    images = [
+        'https://qesot.com/img_dir/cars/9843-bmw-6-series-e63-2005-1.jpeg',
+        'https://qesot.com/img_dir/cars/9843-bmw-6-series-e63-2005-2.jpeg',
+        'https://qesot.com/img_dir/cars/9843-bmw-6-series-e63-2005-3.jpeg',
+        'https://qesot.com/img_dir/cars/9843-bmw-6-series-e63-2005-4.jpeg',
+        'https://qesot.com/img_dir/cars/9843-bmw-6-series-e63-2005-5.jpeg',
+        'https://qesot.com/img_dir/cars/9843-bmw-6-series-e63-2005-6.jpeg',
+        'https://qesot.com/img_dir/cars/9843-bmw-6-series-e63-2005-7.jpeg',
+        'https://qesot.com/img_dir/cars/9843-bmw-6-series-e63-2005-8.jpeg'
+      ]
+    // images.map(async img=>{
+    //     img = await imageConvert(img)
+    // })
+    result = await imageConvert(images[0])
     console.log(result)
-    await manager.add(result)
-})
+}
+processing()
